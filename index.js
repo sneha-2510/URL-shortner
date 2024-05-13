5bff88234b1f5d43cefbe9a6ff1ce88323a9d295
sneha-2510 <ranisnehaaa@gmail.com>
2024-05-14 00:39:32 +0530

    url shortning service done

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const urlRouter = require('./routes/url.js')
+const connectToMongoDB = require("./connection.js")
+const URL = require('./models/url.js')
 
 const app = express();
 const PORT = 8001
 
+connectToMongoDB("mongodb://localhost:27017/urlShortner")
+
+app.use(express.json())
+
+app.use("/url", urlRouter)
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamps: Date.now(),
+                }
+            }
+        })
+
+    res.redirect(entry.redirectURL)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
