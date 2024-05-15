82885d4ab6192774e0bdf7fbcb64c14059a44fb6
sneha-2510 <ranisnehaaa@gmail.com>
2024-05-15 15:32:20 +0530

    build authentication

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-
+mongoose.set("strictQuery", true);
 
 async function connectToMongoDB(url) {
     return mongoose.connect(url).then(() => console.log("DB connected"))
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const path = require('path')
-const urlRouter = require('./routes/url.js')
-const staticRoute = require('./routes/staticRouter.js')
-const connectToMongoDB = require("./connection.js")
-const URL = require('./models/url.js')
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const connectToMongoDB = require("./connection.js");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = 8001
+const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/urlShortner")
 
-//ejs
-app.set("view engine", "ejs")
-app.set("views", path.resolve("./views"))
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.get("/test", async (req, res) => {
-    const allUrls = await URL.find({});
-    return res.render("home", {
-        urls: allUrls,
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-    })
-})
-
-app.use("/url", urlRouter)
-app.use("/", staticRoute)
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
@@ -42,10 +40,7 @@ app.get("/url/:shortId", async (req, res) => {
             },
         }
     );
-
     res.redirect(entry.redirectURL);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
