d703ff55fd087371deccf69ffc4ff51ef3649aff
sneha-2510 <ranisnehaaa@gmail.com>
2024-05-15 13:08:39 +0530

    server side rendering with ejs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const path = require('path')
 const urlRouter = require('./routes/url.js')
+const staticRoute = require('./routes/staticRouter.js')
 const connectToMongoDB = require("./connection.js")
 const URL = require('./models/url.js')
 
@@ -8,26 +10,41 @@ const PORT = 8001
 
 connectToMongoDB("mongodb://localhost:27017/urlShortner")
 
+//ejs
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+
+    })
+})
 
 app.use("/url", urlRouter)
+app.use("/", staticRoute)
 
-app.get('/:shortId', async (req, res) => {
-    const shortId = req.params.shortId
+app.get("/url/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
-            shortId
+            shortId,
         },
         {
             $push: {
                 visitHistory: {
-                    timestamps: Date.now(),
-                }
-            }
-        })
+                    timestamp: Date.now(),
+                },
+            },
+        }
+    );
 
-    res.redirect(entry.redirectURL)
-})
+    res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
